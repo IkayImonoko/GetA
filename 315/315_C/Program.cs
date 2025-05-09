fb4c6cce452ac4bf9f26a88edf88bbe5ddf8c160
IkayImonoko <ntolok1990@gmail.com>
2025-05-09 14:37:51 +0200

    +Oppgave 315C

---
+++ 315/315_C/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _315_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string argument = args.Length > 0 ? args[0] : "";
+            
+            while (!(argument.Length > 0))
+            {
+                try
+                {
+                    Console.WriteLine("You send no argument, try again");
+                    argument = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(argument))
+                    {
+                        Console.WriteLine("it is null or space");
+                        continue;
+                    }
+                }
+                catch (IOException) { }
+                catch (OutOfMemoryException) { }
+                catch (ArgumentOutOfRangeException) { }
+            }
+            char[] letters = argument.ToCharArray();
+            Array.Reverse(letters);
+            Console.WriteLine(new string(letters));
+        }
+    }
+}
