6199f55541140fc8a73b38fcee523667a9d3ee99
IkayImonoko <ntolok1990@gmail.com>
2025-05-15 14:53:48 +0200

    Oppgave 315F almost done

--- 315/315_F/Board.cs
+++ 315/315_F/Board.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,46 @@ namespace _315_F
     {
         private int _size;
         private int[] _numbers;
+        private int[] _victoryOrder;
         public Board(int size = 3)
         {
             Random random = new Random();
             _size = size;
-            _numbers = Enumerable.Range(0, (_size * _size) + 1).OrderBy(_ => random.Next()).ToArray();
+            _numbers = Enumerable.Range(0, _size * _size).OrderBy(_ => random.Next()).ToArray();
+            _victoryOrder = Enumerable.Range(1, _size * _size - 1).Concat(new[] { 0 }).ToArray();
         }
 
-        public void Draw()
+        public int Size
         {
-            for (int i = 0; i < _size; i++)
+            get { return _size; }
+        }
+
+        public int[] Numbers
+        {
+            get { return _numbers; }
+        }
+
+        public int GetNullIndex()
+        {
+            return Array.IndexOf(_numbers, 0);
+        }
+        public void SetNullIndex(int newIndex)
+        {
+            int oldIndex = GetNullIndex();
+            if (newIndex >= 0 && newIndex <= _size * _size - 1)
             {
-                for (int j = 0; j < _size; j++)
-                {
-                    Console.Write($"{_numbers[_size * i + j]}   ");
-                }
-                Console.Write("\n\n");
+                int temp = _numbers[oldIndex];
+                _numbers[oldIndex] = _numbers[newIndex];
+                _numbers[newIndex] = temp;
             }
+            else
+            {
+                Console.WriteLine("Error: Board: _numbers out of the range");
+            }
+        }
+        public bool IsVictory()
+        {
+            return _numbers.SequenceEqual(_victoryOrder);
         }
     }
 }
--- 315/315_F/Game.cs
+++ 315/315_F/Game.cs
@@ -1,17 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+
 
 namespace _315_F
 {
     internal class Game
     {
+        private static void Draw(Board board)
+        {
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    Console.Write($"{board.Numbers[board.Size * i + j]}   ");
+                }
+                Console.Write("\n\n");
+            }
+        }
+        private static int ParseCoordinatesToArrayIndex(Point coordinates, int boardSize)
+        {
+            return coordinates.Y * boardSize + coordinates.X;
+        }
+
+        private static Point ParseArrayIndexToCoordinates(int index, int boardSize)
+        {
+            Point coordinates = new Point();
+            coordinates.Y = index / boardSize;
+            coordinates.X = index % boardSize;
+            return coordinates;
+        }
+        private static void UpdateView(Board board)
+        {
+            Console.Clear();
+            if (!board.IsVictory())
+            {
+                Draw(board);
+            }
+            else
+            {
+                Console.WriteLine("You Winn!!!");
+            }
+        }
+
+        private static void MoveEmptyField(ref Point currentPosition, int bordSize, string direction)
+        {
+            switch (direction)
+            {
+                case "Up":
+                    if (currentPosition.Y > 0)
+                    {
+                        currentPosition.Y -= 1;
+                    }
+                    break;
+                case "Down":
+                    if (currentPosition.Y < bordSize - 1)
+                    {
+                        currentPosition.Y += 1;
+                    }
+                    break;
+                case "Left":
+                    if (currentPosition.X > 0)
+                    {
+                        currentPosition.X -= 1;
+                    }
+                    break;
+                case "Right":
+                    if (currentPosition.X < bordSize - 1)
+                    {
+                        currentPosition.X += 1;
+                    }
+                    break;
+            }
+        }
         public static void Run()
         {
-            Board board = new Board();
-            board.Draw();
+            Board board = new Board(2);
+            Draw(board);
+
+            var emptyFieldCoordinates = ParseArrayIndexToCoordinates(board.GetNullIndex(), board.Size);
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        MoveEmptyField(ref emptyFieldCoordinates, board.Size, "Up");
+                        board.SetNullIndex(ParseCoordinatesToArrayIndex(emptyFieldCoordinates, board.Size));
+                        break;
+                    case ConsoleKey.DownArrow:
+                        MoveEmptyField(ref emptyFieldCoordinates, board.Size, "Down");
+                        board.SetNullIndex(ParseCoordinatesToArrayIndex(emptyFieldCoordinates, board.Size));
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        MoveEmptyField(ref emptyFieldCoordinates, board.Size, "Left");
+                        board.SetNullIndex(ParseCoordinatesToArrayIndex(emptyFieldCoordinates, board.Size));
+                        break;
+                    case ConsoleKey.RightArrow:
+                        MoveEmptyField(ref emptyFieldCoordinates, board.Size, "Right");
+                        board.SetNullIndex(ParseCoordinatesToArrayIndex(emptyFieldCoordinates, board.Size));
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        break;
+                }
+
+                UpdateView(board);
+            }
         }
     }
 }
