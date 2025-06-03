47fbc08a62f301958b72aef846a36ec3d174b288
IkayImonoko <ntolok1990@gmail.com>
2025-06-03 22:17:33 +0200

    Oppgave 321A

--- 321/A/CharCounter.cs
+++ 321/A/CharCounter.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace A
 {
     internal class CharCounter
     {
+        private int[] _counts;
+        private string _text;
+
+        public CharCounter(string text  = "")
+        {
+            var range = 250;
+            _counts = new int[range];
+            _text = text;
+        }
+
+        public void SetText(string text)
+        {
+            _text = text;
+        }
+
+        public void Count()
+        {
+            if(_text.Length > 0)
+            {
+                foreach (var character in _text)
+                {
+                    _counts[(int)character]++;
+                }
+            }
+        }
+
+        public void Show()
+        {
+            var range = 250;
+            for (var i = 0; i < range; i++)
+            {
+                if (_counts[i] > 0)
+                {
+                    var character = (char)i;
+                    Console.WriteLine(character + " - " + _counts[i]);
+                }
+            }
+        }
     }
 }
--- 321/A/Program.cs
+++ 321/A/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var counter = new CharCounter();
+            counter.SetText("Ta utgangspunkt i koden");
+            counter.Count();
+            counter.Show();
         }
     }
 }
